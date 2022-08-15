412097a3929fe6b2a703e1c850c4fb25224d25f5
obeachs <57264251+obeachs@users.noreply.github.com>
2022-08-15 13:56:15 +0100

    initial commit

---
+++ helloworld/helloworld.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  cout << "Giff name and age: ";
+  string name;
+  float age;
+  cin >> name;
+  cout << "Giff egg: ";
+  cin >> age;
+  cout << "Your name is " << name << " and you are " << age*12 << " months old";
+  return 0;
+}
+
+
+
+
